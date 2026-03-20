9a510a224fb62a1e5c454bd9f73a2bdad2816afb
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 09:49:14 +0530

    UC11: OOP booking service layer

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,16 @@
+import java.util.*;
+
+class BookingService {
+    Map<Integer, String> bookings = new HashMap<>();
+
+    void book(int room, String name) {
+        bookings.put(room, name);
+    }
+}
+
 public class HotelBookingApp {
     public static void main(String[] args) {
-        String name = "JOHN".toLowerCase();
-        System.out.println(name);
+        BookingService service = new BookingService();
+        service.book(101, "John");
     }
 }
