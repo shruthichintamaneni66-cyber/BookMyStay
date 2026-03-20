8cd8d95778444854493f3f8e951cd498af16ffc1
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 09:47:34 +0530

    UC10: Case-insensitive handling

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,16 +1,6 @@
-import java.util.*;
-
 public class HotelBookingApp {
-
-    static boolean search(List<Integer> list, int i, int target) {
-        if (i >= list.size()) return false;
-        if (list.get(i) == target) return true;
-        return search(list, i + 1, target);
-    }
-
     public static void main(String[] args) {
-        List<Integer> rooms = Arrays.asList(101, 102, 103);
-
-        System.out.println(search(rooms, 0, 102));
+        String name = "JOHN".toLowerCase();
+        System.out.println(name);
     }
 }
