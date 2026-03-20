0a96def3d33e36f607d3558ed34684eaa7918245
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 09:46:22 +0530

    UC9: Recursive room search

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,12 +1,16 @@
 import java.util.*;
 
 public class HotelBookingApp {
+
+    static boolean search(List<Integer> list, int i, int target) {
+        if (i >= list.size()) return false;
+        if (list.get(i) == target) return true;
+        return search(list, i + 1, target);
+    }
+
     public static void main(String[] args) {
-        LinkedList<String> history = new LinkedList<>();
+        List<Integer> rooms = Arrays.asList(101, 102, 103);
 
-        history.add("Room booked");
-        history.add("Room cancelled");
-
-        System.out.println(history);
+        System.out.println(search(rooms, 0, 102));
     }
 }
