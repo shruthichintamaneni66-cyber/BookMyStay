a15be9ea590f06d20a77f65901660f41b84c3c80
shruthi.chintamaneni66 <shruthi.chintamaneni66@gmail.com>
2026-03-20 09:39:10 +0530

    UC4: FIFO booking using Queue

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -2,15 +2,11 @@ import java.util.*;
 
 public class HotelBookingApp {
     public static void main(String[] args) {
-        Set<Integer> bookedRooms = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
 
-        int room = 101;
+        queue.add("Customer1");
+        queue.add("Customer2");
 
-        if (!bookedRooms.contains(room)) {
-            bookedRooms.add(room);
-            System.out.println("Room booked");
-        } else {
-            System.out.println("Already booked");
-        }
+        System.out.println("Serving: " + queue.poll());
     }
 }
